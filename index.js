01362e8cb8dc3de39c28b4dbf46cdf93df297f12
Blake Tarter <blake@codekoalas.com>
2017-02-19 12:53:44 -0600

    removed all postfix operators, also polyfilled __proto__

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+/* eslint-disable */
+// Only works in Chrome and FireFox, does not work in IE:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/setPrototypeOf
+function setPrototypeOf(obj, proto) {
+  if (Object.setPrototypeOf) {
+    return Object.setPrototypeOf(obj, proto);
+  }
+
+  obj.__proto__ = proto;
+  return obj;
+}
+/* eslint-enable */
+
 const fp = function fpFn(list) {
   const initialList = list.slice(0);
-  initialList.__proto__ = fp.prototype;
-  return initialList;
+  return setPrototypeOf(initialList, fp.prototype);
 };
 
-fp.prototype = new Array();
+fp.prototype = [];
 
 fp.prototype.forEach = function forEach(projectionFunction) {
-  for (let i = 0, ii = this.length; i < ii; i++) {
+  for (let i = 0, ii = this.length; i < ii; i += 1) {
     projectionFunction(this[i]);
   }
   return this;
@@ -15,7 +27,7 @@ fp.prototype.forEach = function forEach(projectionFunction) {
 
 fp.prototype.map = function map(projectionFunction) {
   const newList = fp([]);
-  for (let i = 0, ii = this.length; i < ii; i++) {
+  for (let i = 0, ii = this.length; i < ii; i += 1) {
     newList.push(projectionFunction(this[i]));
   }
   return newList;
@@ -23,7 +35,7 @@ fp.prototype.map = function map(projectionFunction) {
 
 fp.prototype.filter = function filter(predicateFunction) {
   const newList = fp([]);
-  for (let i = 0, ii = this.length; i < ii; i++) {
+  for (let i = 0, ii = this.length; i < ii; i += 1) {
     if (predicateFunction(this[i])) {
       newList.push(this[i]);
     }
@@ -57,7 +69,7 @@ fp.prototype.reduce = function reduce(combiner, initialValue) {
 
 fp.prototype.concatAll = function concatAll() {
   const newList = fp([]);
-  for (let i = 0, ii = this.length; i < ii; i++) {
+  for (let i = 0, ii = this.length; i < ii; i += 1) {
     newList.push(...this[i]);
   }
 
@@ -66,7 +78,7 @@ fp.prototype.concatAll = function concatAll() {
 
 fp.prototype.concatMap = function concatMap(projectionFunction) {
   const newList = fp([]);
-  for (let i = 0, ii = this.length; i < ii; i++) {
+  for (let i = 0, ii = this.length; i < ii; i += 1) {
     newList.push(projectionFunction(this[i]));
   }
   return newList.concatAll();
@@ -75,7 +87,7 @@ fp.prototype.concatMap = function concatMap(projectionFunction) {
 fp.zip = function zip(leftList, rightList, combiner) {
   const newList = fp([]);
 
-  for (let i = 0, ii = Math.min(leftList.length, rightList.length); i < ii; i++) {
+  for (let i = 0, ii = Math.min(leftList.length, rightList.length); i < ii; i += 1) {
     newList.push(combiner(leftList[i], rightList[i]));
   }
 
